fdb0dfd3970f54f74c2df3b02cbb234bdcac7297
giadantioco <giada.antioco@gmail.com>
2024-09-10 15:05:53 +0200

    feat: add login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,74 @@
 import { login } from "./actions";
+import Image from "next/image";
 
 export default function LoginPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-    </form>
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <Image
+            width={100}
+            height={100}
+            src={"/img/logoipsum-332.svg"}
+            alt={"logo"}
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  autoComplete="email"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Password
+              </label>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  autoComplete="current-password"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                formAction={login}
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,72 @@
-export default function Home() {
-  return (
-    <div></div>
-  );
+import { redirect } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
+import { revalidatePath } from "next/cache";
+
+export default async function Home() {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  console.log("user:", user);
+
+  if (!user) {
+    revalidatePath("/", "layout");
+    redirect("/login");
+  }
+
+  // const { data: profileWithRole, error } = await createClient()
+  //   .from("profiles")
+  //   .select("role") // Accedi al nome del ruolo direttamente dalla tabella roles
+  //   .eq("id", user.id)
+  //   .limit(1)
+  //   .maybeSingle();
+
+  // if (error) {
+  //   console.error("Error fetching profile with role:", error);
+  //   return;
+  // }
+
+  // if (profileWithRole) {
+  //   const roleName = profileWithRole;
+  //   console.log("Role:", roleName); // Restituisce 'teacher' o 'student'
+  // }
+
+  // const { data: role, errorRole } = await createClient()
+  //   .from("roles")
+  //   .select("role") // Accedi al nome del ruolo direttamente dalla tabella roles
+  //   .eq("role", profileWithRole?.role)
+  //   .limit(1)
+  //   .maybeSingle();
+
+  // if (errorRole) {
+  //   console.error("Error fetching profile with role:", errorRole);
+  //   return;
+  // }
+
+  // if (role) {
+  //   const roleName = role;
+  //   console.log("Role:", roleName); // Restituisce 'teacher' o 'student'
+  // }
+
+  // // console.log(user.role);
+  // // const pippo = await createClient()
+  // //   .from("profiles")
+  // //   .select("r.role") // Aggiungi la relazione con la tabella roles
+  // //   .eq("id", user.id)
+  // //   .limit(1)
+  // //   .maybeSingle();
+
+  // // console.log("pippo", pippo);
+
+  // //   SELECT
+  // //   r.role
+  // // FROM
+  // //   profiles p
+  // //   JOIN roles r ON p.role = r.id
+  // // WHERE
+  // //   p.id = 'e716b89e-0827-4c9f-81ee-b9cf9479145e';
+
+  return <main>Hello, weschool!</main>;
 }
