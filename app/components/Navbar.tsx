41b35e626e48b22d2e785c4e04fa855d0b176a8d
Ericaruso <ericacaruso024@gmail.com>
2024-09-11 13:31:53 +0200

    feat: Add Button components and add icons and other

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,20 +3,25 @@ import Link from "next/link";
 
 const Navbar: React.FC = () => {
     return (
-        <div className="w-full text-gray-700 bg-white shadow-md">
+        <div className="w-full text-gray-700 bg-white shadow-md ">
             <div className="container mx-auto flex flex-col md:flex-row items-center justify-between p-4">
-                <div className="text-lg font-semibold tracking-widest text-gray-900 uppercase rounded-lg">
+                <div className="text-lg font-semibold tracking-widest text-gray-900 uppercase rounded-lg ">
                     <Link href="/">WeSchool</Link>
                 </div>
                 <nav className="flex space-x-4">
-                    <Link href="" className="text-gray-800">
-                        Classi
+                    <Link href="">
+                        <img 
+                            src="/img/promotion.png" 
+                            alt="Comunicazioni" 
+                            className="w-6 h-6 hover:opacity-75"
+                        />
                     </Link>
-                    <Link href="" className="text-gray-800">
-                        Studenti
-                    </Link>
-                    <Link href="" className="text-gray-800">
-                        Comunicazioni
+                    <Link href="">
+                        <img 
+                            src="/img/settings.png" 
+                            alt="Impostazioni" 
+                            className="w-6 h-6 hover:opacity-75"
+                        />
                     </Link>
                 </nav>
             </div>
