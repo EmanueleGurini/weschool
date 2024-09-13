3bc59ff2bf44edef831bccb1600ec49d4473261d
giadantioco <giada.antioco@gmail.com>
2024-09-13 19:21:25 +0200

    feat: add style to login & account pages

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,67 +5,68 @@ import Image from "next/image";
 export default function LoginPage() {
   return (
     <>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <Image
-            width={100}
-            height={100}
-            src={"/img/logoipsum-332.svg"}
-            alt={"logo"}
-          />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+      <div className="flex min-h-screen items-center justify-center p-6 bg-gray-100">
+        <div className="max-w-md w-full bg-white p-8 rounded-lg shadow-lg space-y-6">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm flex justify-center">
+            <Image
+              width={100}
+              height={100}
+              src={"/img/logoipsum-332.svg"}
+              alt={"logo"}
+            />
+          </div>
+
+          <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-color100">
             Sign in to your account
           </h2>
-        </div>
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6">
+
+          <form className="">
             <div>
               <label
                 htmlFor="email"
-                className="block text-sm font-medium leading-6 text-gray-900"
+                className="block pt-4 pb-1 text-sm font-medium text-gray-700"
               >
                 Email address
               </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  required
-                  autoComplete="email"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                required
+                autoComplete="email"
+                className="mt-1  block w-full rounded-md border-0 py-3 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
             </div>
+
             <div>
               <label
                 htmlFor="password"
-                className="block text-sm font-medium leading-6 text-gray-900"
+                className="block pt-4 pb-1 text-sm font-medium text-gray-700"
               >
                 Password
               </label>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  required
-                  autoComplete="current-password"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                required
+                autoComplete="current-password"
+                className="mt-1 block w-full rounded-md border-0 py-3 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
             </div>
-            <Link 
-              href="/retrievePasswordEmail"
-              className="text-sm font-medium text-blue-500 underline"
-            >
-              Retrieve Password
-            </Link>
+            <div className="pt-3 pb-4">
+              <Link
+                href="/retrievePasswordEmail"
+                className="text-sm font-bold text-color100 underline"
+              >
+                Retrieve Password
+              </Link>
+            </div>
             <div>
               <button
                 formAction={login}
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="w-full bg-color100 text-color0 py-2 rounded-md hover:bg-color80 focus:outline-none focus:ring-2  focus:ring-offset-2"
               >
                 Sign in
               </button>
