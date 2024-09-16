1decbcf76ae7843e17cc5f2f4043f71818651bde
Ciaranatalie <chiara.rapisarda29@gmail.com>
2024-09-16 09:55:41 +0200

    add style to teacher pages

--- app/dashboard/teacher/page.tsx
+++ app/dashboard/teacher/page.tsx
@@ -6,6 +6,8 @@ import { IRole } from "app/page";
 import { getAvatarImg, getTeacherDataByID } from "app/api/supabase/actions";
 import Link from "next/link";
 import Image from "next/image";
+import { table } from "console";
+import Button from "@/components/ui/Button";
 
 interface ICourses {
   course: string;
@@ -53,35 +55,46 @@ export default async function TeacherPage() {
   const today = new Date();
 
   return (
-    <div className="container mx-auto p-6">
-      {imgUrl.publicUrl !== "https://ihymhmvbzbgzrnlusnxj.supabase.co/storage/v1/object/public/avatars/null" && <Image src={imgUrl.publicUrl} alt="Teacher Avatar" width={200} height={200} className="rounded-full mb-4"/>}
+      <div className="container mx-auto p-6">
+      {imgUrl.publicUrl !== "https://ihymhmvbzbgzrnlusnxj.supabase.co/storage/v1/object/public/avatars/null" && <Image src={imgUrl.publicUrl} alt="Teacher Avatar" width={150} height={150} className="rounded-full mb-4"/>}
       <div className="w-full flex items-center justify-between font-extrabold">
         <h2 className="text-2xl font-bold mb-4">Hi, {dataTeacher.full_name}!</h2>
         <p>{formatDate(today)}</p>
       </div>
       <p className="text-gray-600 mb-6">I hope you have a good day.</p>
-      <div className="overflow-x-auto">
-        <table className="min-w-full table-auto bg-white shadow-md border border-gray-300 border-collapse rounded-lg">
-          <thead>
-            <tr className="bg-blue-500 text-gray-700">
-              <th className="px-6 py-3 text-left text-sm font-medium border-none text-white">Class Name</th>
-              <th className="px-6 py-3 text-left text-sm font-medium border-none text-white">Student Number</th>
-              <th className="px-6 py-3 text-left text-sm font-medium border-none text-white">Class Details</th>
-            </tr>
-          </thead>
-          <tbody>
-            {dataTeacher.courses.map((element: ICourses, index: number) => (
-              <tr key={index} className="border-b border-gray-300 last:border-none">
-                <td className="px-6 py-4 whitespace-nowrap border border-gray-300">{element.course}</td>
-                <td className="px-6 py-4 whitespace-nowrap border border-gray-300">{element.totalStudents}</td>
-                <td className="px-6 py-4 whitespace-nowrap border border-gray-300">
-                  <Link href={`/dashboard/teacher/class/${element.id}`} className="text-blue-500 hover:underline">Go To Details</Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <div className="container mx-auto px-4 sm:px-8">
+      <div className="py-10">
+      <div className="p-8 bg-lightGray bg-opacity-70 rounded-[10px] shadow-lg">
+                    <div className="overflow-x-auto">
+                        <div className="inline-block min-w-full shadow rounded-lg overflow-hidden bg-white">
+                            <table className="min-w-full leading-normal">
+              <thead>
+                <tr className="bg-primary text-white">
+                  <th className="px-5 py-3 border-b-2 border-gray-200 text-left text-xs font-semibold uppercase tracking-wider">Class Name</th>
+                  <th className="px-5 py-3 border-b-2 border-gray-200 text-left text-xs font-semibold uppercase tracking-wider">Student Number</th>
+                  <th className="px-5 py-3 border-b-2 border-gray-200 text-left text-xs font-semibold uppercase tracking-wider">Class Details</th>
+                </tr>
+              </thead>
+              <tbody>
+                {dataTeacher.courses.map((element: ICourses, index: number) => (
+                  <tr key={index} className="last:rounded-bl-lg last:rounded-br-lg">
+                    <td className="px-6 py-4 whitespace-nowrap border-b border-gray-300">{element.course}</td>
+                    <td className="px-6 py-4 whitespace-nowrap border-b border-gray-300">{element.totalStudents}</td>
+                    <td className="px-6 py-4 whitespace-nowrap border-b border-gray-300">
+                      <Link 
+                      href={`/dashboard/teacher/class/${element.id}`} passHref>
+                      <Button className="w-full">Go To Details</Button>
+                      </Link>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
       </div>
     </div>
-  )
+    </div>
+  );
 }
