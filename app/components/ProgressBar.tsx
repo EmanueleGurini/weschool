67c4095824af693f26a65fca7d71ff9c4016081f
Jiyuun <167774681+Jiyuunn@users.noreply.github.com>
2024-09-16 15:10:57 +0200

    feat:update student page and progress bar

--- app/components/ProgressBar.tsx
+++ app/components/ProgressBar.tsx
@@ -1,21 +1,12 @@
-"use client"
-
-interface ProgressBarProps {
-  value: number;  
-  total: number;   
-  color: string;   
-}
-
-const ProgressBar: React.FC<ProgressBarProps> = ({ value, total, color }) => {
-  const progress = total > 0 ? (value / total) * 100 : 0;
+const ProgressBar = ({ value, total, color }: { value: number, total: number, color: string }) => {
+  const percentage = (value / total) * 100;
 
   return (
-    <div className="w-full bg-gray-200 rounded-full h-4 mb-4">
+    <div className="w-full bg-[#f7f7f8] rounded-full h-6">
       <div
-        className={`h-4 rounded-full transition-all duration-300 ${color}`}
-        style={{ width: `${progress}%` }}
-      ></div>
-      <p className="mt-1 text-sm text-gray-500">{value} / {total}</p>
+        className={`${color} h-6 rounded-full`}
+        style={{ width: `${percentage}%` }}
+      />
     </div>
   );
 };
